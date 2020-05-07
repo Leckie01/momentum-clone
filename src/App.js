ae79c560d529a8b57911a45a3eda6bb5b377ef7a
Leckie <hsk0094@gmail.com>
2020-05-08 02:16:29 +0900

    템플릿 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+
+const MainContainer = styled.div``;
+
+const ClockContainer = styled.div``;
+
+const FormContainer = styled.form``;
+
+const ToDoList = styled.ul``;
 
 const App = () => {
-  return <div></div>;
+  return (
+    <MainContainer>
+      <LocationContainer>
+        <span className="location"></span>
+      </LocationContainer>
+      <ClockContainer>
+        <h1 className="clock-title"></h1>
+      </ClockContainer>
+      <FormContainer>
+        <input
+          className="user-name"
+          placeholder="What is your name?"
+          name="user-name"
+        />
+      </FormContainer>
+      <ToDoList></ToDoList>
+    </MainContainer>
+  );
 };
 
 export default App;
